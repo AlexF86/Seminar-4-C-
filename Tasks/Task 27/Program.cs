5fc4e59354bf8935bcdcba7d7bcaf9a020a5dfcd
Alexandr <don.alexande1986@yandex.ru>
2022-08-23 13:55:16 +0300

    задача 27

--- Tasks/Task 27/Program.cs
+++ Tasks/Task 27/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine();
+int x = Convert.ToInt32(Console.ReadLine());
+int number = x;
+int sumDigit = 0;
+System.Console.Write($"Сумма цифр числа {number} равна: ");
+
+while (number > 0)
+{
+    int digit = number % 10;
+    sumDigit = sumDigit + digit;
+    number = number / 10;
+}
+System.Console.WriteLine(sumDigit);
+System.Console.WriteLine();
