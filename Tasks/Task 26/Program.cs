1501f46cd236379752bf293bfade43007e5f03ff
Alexandr <don.alexande1986@yandex.ru>
2022-08-23 12:27:50 +0300

    задача 26

--- Tasks/Task 26/Program.cs
+++ Tasks/Task 26/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Вводим число x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (x > 0)
+{
+    x /= 10;
+    count++;
+}
+Console.WriteLine($"Количество цифр в числе = {count}");
+
